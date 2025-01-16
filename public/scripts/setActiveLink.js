0a5d5b9ec1f71663af6baf893d78275324ef7887
Dan <dania52@Linux.myguest.virtualbox.org>
2025-01-16 12:15:09 +0200

    update styles, fix in setActiveLink.js

--- public/scripts/setActiveLink.js
+++ public/scripts/setActiveLink.js
@@ -1,11 +1,9 @@
 var pathName = window.location.pathname;
 var headerLinks = document.querySelector(".header-links-list");
-
 var links = Object.values(headerLinks.getElementsByTagName("a"));
-var listElements = Object.values(headerLinks.getElementsByTagName("li"));
 
-links.forEach((link, index) => {
+links.forEach((link) => {
     if (link.getAttribute("href") == pathName) {
-        listElements[index].setAttribute("class", "active-link");
+        link.setAttribute("class", "active-link");
     }
-})
+});
